01e947ed6ae4934249728753acd09a0f8ba19b5b
Ilya <levil133@gmail.com>
2017-09-27 22:10:43 +0300

    Added sort() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        print(args);
+        sort(args);
     }
 
     public static void print(String[] args) {
         for (String arg : args) {
             System.out.println(arg);
         }
+    }
+
+    public static void sort(String[] args){
+        Arrays.sort(args);
+        print(args);
     }
 }

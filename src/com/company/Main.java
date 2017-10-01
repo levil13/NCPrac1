1f995bfd895374da4b98c5d191d588433414098a
Ilya <levil133@gmail.com>
2017-10-01 22:58:30 +0300

    Changed sort() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,14 @@ public class Main {
     }
 
     public static void sort(String[] args) {
-        Arrays.sort(args);
+        for (int i = args.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (args[j].compareTo(args[j + 1]) > 0) {
+                    String temp = args[j];
+                    args[j] = args[j + 1];
+                    args[j + 1] = temp;
+                }
+            }
+        }
     }
 }
